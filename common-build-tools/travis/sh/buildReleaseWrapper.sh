3082e537a70aaec05b1de8a941469a5646dab177
Olivier ORAND <olivier.orand@orange.com>
2016-05-02 17:40:16 +0200

    bump to latest elpaaso-build-tools/master
    commit 9194754c1a663b0d128efda5cd796e105defc2d7
    Author: Olivier ORAND <olivier.orand@orange.com>
    Date:   Mon May 2 17:23:16 2016 +0200
    
        Add auto release using mvnw
    
    commit 85d802d9788a746d47bd51e9618f030e771118b3
    Author: Olivier ORAND <olivier.orand@orange.com>
    Date:   Fri Apr 29 14:27:25 2016 +0200
    
        feat(directly release jar on bintray)
    
    commit 0f97405ecaab7f10e07d0d56f06f48b346390d16
    Author: Olivier ORAND <olivier.orand@orange.com>
    Date:   Mon Feb 8 12:02:31 2016 +0100
    
        rename license to license.md
    
    commit 25351fa7c03f52ecf00c3ca791035fc30fa0549e
    Author: Olivier ORAND <olivier.orand@orange.com>
    Date:   Mon Feb 8 12:01:40 2016 +0100
    
         add license header to travis template and update chmod path

---
+++ common-build-tools/travis/sh/buildReleaseWrapper.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2015-2016 Orange
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -ev
+
+echo "TRAVIS_BRANCH: <$TRAVIS_BRANCH> - TRAVIS_TAG: <$TRAVIS_TAG>"
+
+#Download dependencies
+mvnw -q help:evaluate -Dexpression=project.version --settings settings.xml
+# Capture execution of maven command - It looks like grep cannot be used like this on travis
+export VERSION_SNAPSHOT=$(mvnw help:evaluate -Dexpression=project.version --settings settings.xml |grep '^[0-9].*')
+
+echo "Current version extracted from pom.xml: $VERSION_SNAPSHOT"
+export VERSION_PREFIX=$(expr "$VERSION_SNAPSHOT" : "\(.*\)-SNAP.*")
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "$TRAVIS_BRANCH" = "master" ]
+then
+	#We are on master without PR
+	export RELEASE_CANDIDATE_VERSION=$VERSION_PREFIX.${TRAVIS_BUILD_NUMBER}
+
+	echo "Release candidate snapshot version: $RELEASE_CANDIDATE_VERSION"
+
+	echo "Setting new version old: $VERSION_SNAPSHOT"
+
+	#Download dependencies
+	mvnw -q release:help --settings settings.xml
+    mvnw --batch-mode -e release:prepare release:perform --settings settings.xml
+
+	echo $RELEASE_CANDIDATE_VERSION > RELEASE_CANDIDATE_VERSION
+else
+	mvnw -q install:help --settings settings.xml
+	mvnw install --settings settings.xml
+fi
+
+
+
